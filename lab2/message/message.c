b8501da91ac2eb59729d5c7fae615cdb620624b5
shatanyumi <1143005768@qq.com>
2020-06-10 21:13:33 +0800

    lab2/

--- lab2/message/message.c
+++ lab2/message/message.c
@@ -7,12 +7,67 @@
 
 #define MAX_TEXT 1024
 
+int exit_sig = 0;
+
 struct msg_st
 {
 	long msg_type;
 	char text[MAX_TEXT];
 };
 
+void server(pid_t pid){
+	struct msg_st data;
+	int msgid = -1;
+	long msgtype = 0;
+	
+	msgid = msgget((key_t)1234,0666 | IPC_CREAT);
+	if(msgid == -1){
+		fprintf(stderr,"msgid get errir\n");
+		return;
+	}
+
+	if(msgrcv(msgid,(void*)&data,BUFSIZ,msgtype,0)<0){
+		fprintf(stderr,"msgrcv failed\n");
+		exit(1);
+	}
+	printf("%s\n",data.text);
+	
+	data.msg_type = 1;
+	sprintf(data.text,"server's process pid is %d",pid);
+	if(msgsnd(msgid,(void*)&data,MAX_TEXT,0)<0){
+		fprintf(stderr,"msgsnd failed\n");
+		exit(1);
+	}
+
+	return;
+	
+}
+
+void client(pid_t pid){
+	struct msg_st data;
+	int msgid = -1;
+	long msgtype = 0;
+	
+	msgid = msgget((key_t)1234,0666|IPC_CREAT);
+	if(msgid == -1){
+		fprintf(stderr,"msgid get error\n");
+		return;
+	}
+	data.msg_type = 1;
+	sprintf(data.text,"client's process pid is %d",pid);
+	if(msgsnd(msgid,(void *)&data,MAX_TEXT,0)<0){
+		fprintf(stderr,"msgsnd failed\n");
+		exit(1);
+	}
+	
+	if(msgrcv(msgid,(void*)&data,BUFSIZ,msgtype,0)<0){
+		fprintf(stderr,"msgrcv failed\n");
+		exit(1);
+	}
+	printf("%s\n",data.text);	
+	exit_sig = 1;
+	return;
+}
 int main(){
 	int running = 1;
 	struct msg_st data;
@@ -25,51 +80,16 @@ int main(){
 	
 	}
 	else if(pid == 0){
-		msgid = msgget((key_t)1234,0666|IPC_CREAT);
-		if(msgid<0){
-			fprintf(stderr,"msgget failed with errir %d\n",errno);
-			exit(EXIT_FAILURE);
+		printf("Child process as server:\n");
+		while(!exit_sig){
+			server(getpid());
 		}
-		while(running){
-			printf("Enter some text:\n");
-			fgets(buffer,BUFSIZ,stdin);
-			data.msg_type = 1;
-			sprintf(data.text,"Child process %d write message: %s",getpid(),buffer);
-			if(msgsnd(msgid,(void*)&data,MAX_TEXT,0) < 0){
-				fprintf(stderr,"msgnd failed\n");
-				exit(EXIT_FAILURE);
-			}
-			if(strncmp(buffer,"end",3)==0){
-				running = 0;
-			}
-			sleep(1);
-		}
-		exit(EXIT_SUCCESS);
-		
+		exit(0);
 	}
 	else{
-		waitpid(pid,NULL,0);	
-		msgid = msgget((key_t)1234,0666 | IPC_CREAT);
-		if(msgid < 0){
-			fprintf(stderr,"msgget failed with error: %d\n",errno);
-			exit(EXIT_FAILURE);
-		}
-		
-		while(running){
-			if(msgrcv(msgid,(void*)&data,BUFSIZ,msgtype,0)<0){
-				fprintf(stderr,"msgrcv failed with errno: %d\n",errno);
-				exit(EXIT_FAILURE);
-			}
-			printf("Parent process recieve: \nYou wrote:%s\n",data.text);
-			if(strncmp(data.text,"end",3) == 0){
-				running = 0;
-			}
-		}
-		if(msgctl(msgid,IPC_RMID,0) < 0){
-			fprintf(stderr,"msgctl(IPC_RMID) failed\n");
-			exit(EXIT_FAILURE);
-		}
-		exit(EXIT_SUCCESS);	
+		printf("Parent process as client:\n");
+		client(getpid());
+		exit(0);
 	}
 	return 0;
 }
